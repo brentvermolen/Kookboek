89491b57fdc6ce082ac11b182ac6671a45f6db67
Brent Vermolen <brentvermolen@hotmail.be>
2019-02-14 11:43:47 +0100

    DAL

---
+++ DAL/EF/KookboekContext.cs
@@ -0,0 +1,49 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.EF
+{
+    public class KookboekContext : DbContext
+    {
+        public KookboekContext() : base("KookboekDb")
+        {
+            Database.SetInitializer(new KookboekInitializer());
+            Database.Initialize(false);
+        }
+
+        public DbSet<Recept> Recepten { get; set; }
+        public DbSet<Ingrediënt> Ingrediënten { get; set; }
+        public DbSet<Bereiding> Bereidingen { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<IngrediëntEenheid>()
+                .HasRequired(i => i.Recept)
+                .WithMany(r => r.Ingrediënten)
+                .HasForeignKey(i => i.ReceptID);
+            modelBuilder.Entity<IngrediëntEenheid>()
+                .HasRequired(i => i.Ingrediënt)
+                .WithMany(i => i.Usages)
+                .HasForeignKey(i => i.IngredientID);
+
+            modelBuilder.Entity<Tag>()
+                .HasMany(t => t.Recepten)
+                .WithMany(r => r.Tags);
+
+            //modelBuilder.Entity<Archief>()
+            //.HasMany(a => a.Films)
+            //.WithMany(f => f.Archieven);
+            //modelBuilder.Entity<Archief>()
+            //   .HasMany(a => a.Afleveringen)
+            //   .WithMany(s => s.Archieven);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ DAL/EF/KookboekInitializer.cs
@@ -0,0 +1,83 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace DAL.EF
+{
+    public class KookboekInitializer : DropCreateDatabaseIfModelChanges<KookboekContext>
+    {
+        protected override void Seed(KookboekContext context)
+        {
+            InsertTags(context);
+            context.SaveChanges();
+
+            InsertTest(context);
+            context.SaveChanges();
+        }
+
+        private void InsertTest(KookboekContext context)
+        {
+            Ingrediënt sjalot = new Ingrediënt()
+            {
+                Naam = "Sjalot"
+            };
+            Ingrediënt kraanwater = new Ingrediënt()
+            {
+                Naam = "Kraanwater"
+            };
+
+            context.Ingrediënten.Add(sjalot);
+            context.Ingrediënten.Add(kraanwater);
+
+            Bereiding deel1 = new Bereiding()
+            {
+                Volgorde = 0,
+                Omschrijving = "Snijd de sjalot"
+            };
+            Bereiding deel2 = new Bereiding()
+            {
+                Volgorde = 1,
+                Omschrijving = "Snijd het brood"
+            };
+
+            List<Bereiding> bereiding = new List<Bereiding>();
+            bereiding.Add(deel1);
+            bereiding.Add(deel2);
+
+            List<IngrediëntEenheid> ingrediënten = new List<IngrediëntEenheid>();
+            ingrediënten.Add(new IngrediëntEenheid() { Ingrediënt = sjalot, Aantal = 1, Eenheid = Eenheid.Aantal });
+            ingrediënten.Add(new IngrediëntEenheid() { Ingrediënt = kraanwater, Aantal = 150, Eenheid = Eenheid.Milliliter });
+
+            List<Tag> tags = new List<Tag>();
+            tags.Add(new Tag { Naam = "Soep" });
+            tags.Add(context.Tags.Find("Vis"));
+
+            Recept recept1 = new Recept()
+            {
+                Naam = "Bouillabaisse",
+                Omschrijving = "Franse vissoep",
+                ImageUrl = "https://img.static-rmg.be/a/food/image/q75/w640/h400/2666/gebakken-zeetong-met-pasta.jpg",
+                AantalPersonen = 1,
+                DuurInMinuten = 20,
+                Ingrediënten = ingrediënten,
+                Bereiding = bereiding,
+                Tags = tags
+            };
+
+            context.Recepten.Add(recept1);
+        }
+
+        private void InsertTags(KookboekContext context)
+        {
+            context.Tags.Add(new Tag { Naam = "Vlees" });
+            context.Tags.Add(new Tag { Naam = "Vis" });
+            context.Tags.Add(new Tag { Naam = "Wok" });
+            context.Tags.Add(new Tag { Naam = "Dessert" });
+            context.Tags.Add(new Tag { Naam = "Voorgerecht" });
+            context.Tags.Add(new Tag { Naam = "Hoofdgerecht" });
+            context.Tags.Add(new Tag { Naam = "Apperitief" });
+            context.Tags.Add(new Tag { Naam = "Drankje" });
+        }
+    }
+}
